347119d992493f23501801ea39a4227cc5562c46
ElimuStuart <stuartelimu@gmail.com>
2019-07-30 13:43:22 +0300

    added custom action to accept refund

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Item, Order, OrderItem, Coupon, Refund
 
-# Register your models here.
+def accept_refund(modeladmin, request, queryset):
+    queryset.update(refund_requested=False, refund_granted=True)
+
+accept_refund.short_description = "Accept Refund Request"
+
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
@@ -14,6 +18,7 @@ class OrderAdmin(admin.ModelAdmin):
     search_fields = ['user__username', 'ref_code']
     list_display_links = ['user', 'coupon', 'billing_address']
     list_filter = ['ordered', 'being_delivered', 'received', 'refund_requested', 'refund_granted']
+    actions=[accept_refund]
 
 admin.site.register(OrderItem)
 admin.site.register(Coupon)
